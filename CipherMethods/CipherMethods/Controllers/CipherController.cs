e725d9d687a39a0074d592ba4a359fb9fc19c2ea
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2020-03-26 17:41:56 -0600

    Creación de matriz para llenar el texto en forma de zigzag

---
+++ CipherMethods/CipherMethods/Controllers/CipherController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.IO;
+
+namespace CipherMethods.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CipherController : ControllerBase
+    {
+        // GET: api/Cipher
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Cipher/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        //cesar y cifrado de ruta
+        // POST: api/Cipher
+        [HttpPost("{name}/{fileName}/{param}")]
+        public void Post([FromForm(Name ="file")] IFormFile file, string name, string fileName, string param)
+        {
+            //lectura del archivo
+            var result = new StringBuilder();
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                    result.AppendLine(reader.ReadLine());
+            }
+
+            if (name.ToLower().Equals("zigzag"))
+            {
+
+            }
+            else
+            {
+                // es cifrado cesar 
+            }
+
+        }
+
+        //zigzag
+        [HttpPost("{name}/{fileName}/{param}")]
+        public void Post([FromForm(Name = "file")] IFormFile file, string name, string fileName, int param)
+        {
+            //lectura del archivo
+            var result = new StringBuilder();
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                    result.AppendLine(reader.ReadLine());
+            }
+
+            ZigZag cipherZigZag = new ZigZag();
+            cipherZigZag.calculate(param, result.ToString(), fileName);
+
+        }
+
+        // PUT: api/Cipher/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CipherMethods/CipherMethods/ZigZag.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CipherMethods
+{
+    public class ZigZag
+    {
+        string fileName { get; set; }
+        int rails { get; set; }
+        public void calculate(int rails, string text, string fileName) {
+            this.fileName = fileName;
+            this.rails = rails;
+            int charQty = 1 + 1 + (2 * (rails - 2)); //cantidad de caracteres por ola 
+            float unit = charQty / (charQty * charQty); //decimal correspondiente a un caracter
+            float waves = text.Length / charQty;
+            double rest = waves  - Math.Truncate(waves); //parte decimal de las olas 
+            
+            rest = 1 - rest; //completar 
+            rest = rest / unit; //cantidad de caracteres especiales que hay que completar
+
+            for (int i = 0; i < rest; i++)
+            {
+                text += "#"; //para completar la ola
+            }
+
+            fillMatrix(rails, text);
+
+        }
+        public void fillMatrix(int rails, string text) {
+            string[,] matrix = new string[rails, text.Length];
+            int cont = 0;
+            while (cont != text.Length) //recorrer toda la cadena
+            {                
+                for (int j = 0; j < rails; j++)
+                {
+                    matrix[j, cont] = text[cont].ToString();
+                    cont++;
+                }
+
+                for (int i = rails - 2; i > 0; i--)
+                {
+                    matrix[i, cont] = text[cont].ToString();
+                    cont++;
+                }
+                                            
+            }
+
+            
+        }
+
+        public void cipher(string[,] matrix) {
+            string encryptedText = "";
+            string folder = @"C:\Cipher\";
+            string fullPath = folder + fileName;
+
+            for (int i = 0; i < rails; i++)
+            {
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    if (matrix[i, j] != "")
+                    {
+                        encryptedText += matrix[i, j];
+                    }
+                }
+            }
+
+
+        }
+    }
+}
